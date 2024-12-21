dbebd5468fba3187045b95737832c55d6fa82f7d
Chad Bojelador <slsuls.chadbojelador@gmail.com>
2024-12-21 15:32:18 +0800

    practice

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+typedef std::string text_t;
+typedef int number_t;
+int main (){
+    
+    text_t firstName = "Chad";
+    number_t 
+    std::cout<<firstName << '\n';
+
+    return 0;
+}
