c36f99f94f82d3b145fbed5f09633d2624f47297
Chad Bojelador <slsuls.chadbojelador@gmail.com>
2025-01-06 23:50:03 +0800

    c

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,40 +3,8 @@
 
 int main(){
 
-    char op;
-    double num1;
-    double num2;
-    double answer;
+    int number = 60;
 
-    std:: cout <<"*****Calculator*****"<<'\n';
+    number % 2 == 60 ? std::cout<< "equal" : std::cout<< "not equal";
 
-    std:: cout <<"enter first number"<<'\n';
-    std::cin>>num1;
-
-    std:: cout <<"enter operation"<<'\n';
-    std::cin>>op;
-
-    std:: cout <<"enter second number"<<'\n';
-    std::cin>>num2;
-
-    if (op == '+') {
-        answer = num1 + num2;
-    }
-
-    else if (op == '-') {
-        answer = num1 - num2;
-    }
-    else if (op == '/') {
-        answer = num1 / num2;
-    }    
-    else if (op == '*') {
-        answer = num1 * num2;
-    }
-
-    else{
-        std::cout<< "its not a valid operation"<< '\n';
-    }
-
-    std::cout<< "The answer is "<<answer<<'\n';
-    std:: cout <<"*****************************"<<'\n';
 }
