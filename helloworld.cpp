2adde5d974d6cf61afda6160b55245b3ac28c3a4
Chad Bojelador <slsuls.chadbojelador@gmail.com>
2025-01-08 12:59:51 +0800

    cc

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,15 +4,26 @@
 int main(){
 
     int temp;
+    bool sunny = true;
+
 
     std::cout << "Enter the temperature";
     std::cin >> temp;
 
-    if (temp > 0 && temp < 30)
+    if (temp <= 0 || temp >= 30)
     {
-       std::cout<<"The temp is good"; /* code */
+       std::cout<<"The temp is bad"; /* code */
     }
     else{
-        std::cout<<"The temp is bad";
+        std::cout<<"The temp is good";
     }
+
+    if (!sunny)
+    {
+        std::cout << "it is not sunny";
+    }
+    else{
+        std::cout << "it is sunny";
+    }
+    
 }   
