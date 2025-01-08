11d1b64c6f35ef925e6216b8f7f7bbb5be9d5d10
Chad Bojelador <slsuls.chadbojelador@gmail.com>
2025-01-08 13:38:10 +0800

    c

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,11 +4,22 @@
 int main (){
 
     double temp;
-    double conversion;
-    double convertedTemp;
+    char conversion;
+    double Celcius = (temp*9/5)+32;
+    double Fahrenheit = (temp-32)*5/9;
 
-    std::cout << "Enter F-C or C-F";
+    std::cout << "Enter the temp: "<< '\n';
+    std::cin >> temp;
+
+    std::cout << "Enter C to convert to Celcius and F to convert to Fahrenheit";
     std::cin >> conversion;
 
-
+    switch (conversion){
+        case 'C':
+        Celcius;
+        break;
+        case 'F':
+        Fahrenheit;
+        break;
+    }
 }
