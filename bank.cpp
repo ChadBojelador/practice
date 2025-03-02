bd2116a79c8d096222813f7a71d45491427b74c0
Chad Bojelador <slsuls.chadbojelador@gmail.com>
2025-03-03 07:27:01 +0800

    yes

---
+++ bank.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+void openAcc();
+
+int main (){
+
+    cout<<"*******************Hi! Welcome to Gcash*******************\n\n"<<"**********Login to an existing account or Create new?**********\n";
+    cout<<"1.Login\n"<<"2.Signup";
+
+    
+    
+    
+    
+    
+    
+    /*
+    double balance;
+
+    cout<<"Choose an option: \n"<<"1. Show Balance\n" <<"2. Deposit Balance\n"<<"3. Withdraw Balance\n"<<"4. Exit\n";
+*/
+    return 0;
+
+}
+
+void openAcc(){
+
+}
