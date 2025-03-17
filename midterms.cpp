9e515e2a0b92829d1f0cd6a667352bef4367ed8d
Chad Bojelador <slsuls.chadbojelador@gmail.com>
2025-03-17 17:02:59 +0800

    dsda

---
+++ midterms.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+void fun(int n){
+    if (n>0)
+    {
+        fun(n-1);
+    }
+    cout<<n<<'\n';
+
+    
+    
+}
+
+
+int main(){
+
+    int x = 3;
+    fun(x);
+
+    
+    return 0;
+}
