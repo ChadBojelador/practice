d416dc41d4b7a91a9fe047f654a36f45607cd8d9
Chad Bojelador <slsuls.chadbojelador@gmail.com>
2025-03-22 13:31:44 +0800

    dada

--- midterms.cpp
+++ midterms.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main (){
+int main() {
+    int x = 3;
+
+    if (x > 5) 
+        cout << "X is greater than 5\n";
+        cout << "This will always execute.\n";
 
     return 0;
 }
